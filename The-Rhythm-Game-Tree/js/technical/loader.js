414608bca84a4b08c0fc46f9e9b94ced9dd2053f
QqQe308 <147911774+QqQe308@users.noreply.github.com>
2023-10-22 19:51:32 +0800

    qwq

--- The-Rhythm-Game-Tree/js/technical/loader.js
+++
@@ -1,9 +0,0 @@
-// Load files
-
-for (file in modInfo.modFiles) {
-    let script = document.createElement("script");
-    script.setAttribute("src", "js/" + modInfo.modFiles[file]);
-    script.setAttribute("async", "false");
-    document.head.insertBefore(script, document.getElementById("temp"));
-}
-
