7ef9a2c7d8f6dfd51f06a8d1fb8aee92b3829c94
QqQe308 <147911774+QqQe308@users.noreply.github.com>
2024-10-20 15:32:44 +0800

    Add files via upload

--- js/layers.js
+++ js/layers.js
@@ -2044,11 +2044,11 @@ return eff
       canClick() {return true},
       onClick() {
        let pttMax=player.l.points.min(35).add(1).log(10).add(1).pow(2).sub(player.a.pttMax.mul(player.a.pttMax2)).div(2).max(0)
-       player.a.pttMax = player.a.pttMax.add(pttMax).min(tmp.ri.ric1)
+       player.a.pttMax = player.a.pttMax.add(pttMax).min(tmp.ri.arc1)
       },
       onHold() {
        let pttMax=player.l.points.min(35).add(1).log(10).add(1).pow(2).sub(player.a.pttMax.mul(player.a.pttMax2)).div(2).max(0)
-        player.a.pttMax = player.a.pttMax.add(pttMax).min(tmp.ri.ric1)
+        player.a.pttMax = player.a.pttMax.add(pttMax).min(tmp.ri.arc1)
       },
       unlocked() {return hasMilestone('l',2)||hasMilestone('p',1)}
     },
